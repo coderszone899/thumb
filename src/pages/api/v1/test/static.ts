99a8262d24ee05a557dd4ea59f0ea1322f1440c1
lezboro213 <lezrel.boro@yahoo.com>
2025-03-26 01:17:34 +0000

    Document README - adding new feature

--- src/pages/api/v1/test/static.ts
+++ src/pages/api/v1/test/static.ts
@@ -1,8 +1,8 @@
 import { return200WithObject } from '@services/return-types';
 import type { APIRoute } from "astro";
 
-export const GET: APIRoute = async (context) => {
-    const blogPosts = [
+export let GET: APIRoute = async (context) => {
+    let blogPosts = [
         { id: 1, title: 'First Blog Post', content: 'This is the content of the first blog post.' },
         { id: 2, title: 'Second Blog Post', content: 'This is the content of the second blog post.' },
         { id: 3, title: 'Third Blog Post', content: 'This is the content of the third blog post.' },
