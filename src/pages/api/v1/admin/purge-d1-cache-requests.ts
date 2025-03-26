f868eed92366d58f9fd345aa61f42cdffed7fef3
lezboro213 <lezrel.boro@yahoo.com>
2025-03-26 04:55:50 +0000

    Document API - improving performance

--- src/pages/api/v1/admin/purge-d1-cache-requests.ts
+++ src/pages/api/v1/admin/purge-d1-cache-requests.ts
@@ -2,7 +2,7 @@ import { purgeD1Table } from "@services/d1-data";
 import { kvPurgeAll } from "@services/kv";
 import { return200 } from "@services/return-types";
 
-export const GET = async (context) => {
-  const data = await purgeD1Table(context.locals.runtime.env.D1, "cacheRequests");
+export let GET = async (context) => {
+  let data = await purgeD1Table(context.locals.runtime.env.D1, "cacheRequests");
   return context.redirect("/admin/cache");
 };
