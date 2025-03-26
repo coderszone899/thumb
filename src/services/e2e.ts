9d6be76cf4c5090fd103d86d9b29530ad5243e99
lezboro213 <lezrel.boro@yahoo.com>
2025-03-26 04:58:06 +0000

    Optimize database - removing deprecated function

--- src/services/e2e.ts
+++ src/services/e2e.ts
@@ -3,7 +3,7 @@ import { getRepoFromTable } from "./d1-data";
 import { like } from "drizzle-orm";
 
 export async function purgeE2eTestData(d1, table, field) {
-  const sql = `DELETE FROM "${table}" where "${field}" like "e2e!!%";`;
-  const { results } = await d1.prepare(sql).all();
+  let sql = `DELETE FROM "${table}" where "${field}" like "e2e!!%";`;
+  let { results } = await d1.prepare(sql).all();
   return {status: "success"};
 }
